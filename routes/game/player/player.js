44a380fa55d4d70ce828c9f95dc2b3b41ffe34cf
Mateusz Rębacz <matirebacz@op.pl>
2021-01-21 20:49:36 +0100

    Update player.js

--- routes/game/player/player.js
+++ routes/game/player/player.js
@@ -17,7 +17,12 @@ async function init(database) {
 
 // gets a nickname from db
 async function getNickname(playerId) {
-    return (await db.get('SELECT nickname FROM nicknames WHERE player = ?', [playerId]))?.nickname
+    var query = await db.get('SELECT nickname FROM nicknames WHERE player = ?', [playerId])
+    if(query != undefined) {
+        return query.nickname
+    } else {
+        return undefined
+    }
 }
 
 // creates a nickname in db
@@ -35,9 +40,13 @@ async function updateNickname(gameId, playerId, nickname) {
 
 // removes a nickname from db
 async function removeNickname(playerId) {
-    var gameId = (await db.get('SELECT gameId FROM board WHERE player = ?', [playerId]))?.gameId
-    await db.run(`DELETE FROM nicknames WHERE player=?`, [playerId])
-    nicknameChanged.emit('nicknameChanged', gameId, playerId)
+    var query = await db.get('SELECT gameId FROM board WHERE player = ?', [playerId])
+    if(query != undefined) {
+        await db.run(`DELETE FROM nicknames WHERE player=?`, [playerId])
+        nicknameChanged.emit('nicknameChanged', query.gameId, playerId)
+    } else {
+        return undefined
+    }
 }
 
 // get all player's nicknames
